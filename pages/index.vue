f78fc38e2207ebcff86c6a0f8bcc4533b9d44b47
MT <545176780@qq.com>
2023-03-21 17:16:32 +0800

    feat: 首页显示最近5篇文章

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>homepage</div>
+  <div class="home_container">
+    <div class="recent_container">
+      <div class="recent_tit">Recent artices</div>
+      <div class="artice_container">
+        <ArticesList v-if="data.length !== 0" :data="data"></ArticesList>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+const LIMIT = 5;
+const { data } = await useAsyncData(() => {
+  return queryContent("/posts").sort({ date: -1 }).limit(LIMIT).find();
+});
+</script>
