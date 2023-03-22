96e3b8c9273df08ee330d1a24499ef8456229d42
MT <545176780@qq.com>
2023-03-22 13:00:42 +0800

    feat: 修改首页布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,16 @@
 <template>
   <div class="home_container">
-    <div class="recent_container">
-      <div class="recent_tit">Recent artices</div>
-      <div class="artice_container">
-        <ArticesList v-if="data.length !== 0" :data="data"></ArticesList>
-      </div>
-    </div>
+    <HeroProfile title="Aurora blog" description="知行合一 活在当下" />
+    <RecentArtice></RecentArtice>
   </div>
 </template>
 
-<script setup>
-const LIMIT = 5;
-const { data } = await useAsyncData(() => {
-  return queryContent("/posts").sort({ date: -1 }).limit(LIMIT).find();
-});
-</script>
+<script setup></script>
 
 <style scoped>
-.recent_container {
-  display: flex;
-  justify-content: center;
-  margin-top: 2rem;
+.home_container {
+  max-width: 42rem;
+  margin: 0 auto;
+  padding-bottom: 4rem;
 }
 </style>
